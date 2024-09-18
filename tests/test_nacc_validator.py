63eebf200ed3043a0583b3ddc2fe02f52a8780c8
Emily Cheng <echeng06@uw.edu>
2024-09-18 16:59:12 -0700

    Test and logic case

--- tests/test_nacc_validator.py
+++ tests/test_nacc_validator.py
@@ -326,10 +326,50 @@ def test_logic_or():
     assert nv.validate({'raceaian': 1, 'raceasian': None, 'raceblack': None})
     assert nv.validate({'raceaian': None, 'raceasian': 1, 'raceblack': 1})
     assert nv.validate({'raceaian': 1, 'raceasian': 1, 'raceblack': 1})
+
+    # invalid cases
     assert not nv.validate({'raceaian': None, 'raceasian': None, 'raceblack': None})
     assert nv.errors == {'raceaian': ['error in formula evaluation - value None does not satisfy the specified formula']}
     assert not nv.validate({'raceaian': None})
     assert nv.errors == {'raceaian': ['error in formula evaluation - value None does not satisfy the specified formula']}
+
+def test_logic_and():
+    """ Test mathematical logic and case """
+    schema = {
+        "raceasian": {
+            "type": "integer",
+            "nullable": True,
+            "allowed": [1]
+        },
+        "raceblack": {
+            "type": "integer",
+            "nullable": True,
+            "allowed": [1]
+        },
+        "raceaian": {
+            "type": "integer",
+            "nullable": True,
+            "allowed": [1],
+            "logic": {
+                "formula": {
+                    "and": [
+                        {"==": [1, {"var": "raceaian"}]},
+                        {"==": [1, {"var": "raceasian"}]},
+                        {"==": [1, {"var": "raceblack"}]},
+                    ]
+                }
+            }
+        }
+    }
+
+    nv = create_nacc_validator(schema)
+
+    # only valid case
+    assert nv.validate({'raceaian': 1, 'raceasian': 1, 'raceblack': 1})
+
+    # invalid cases
+    assert not nv.validate({'raceaian': 1, 'raceasian': None, 'raceblack': None})
+    assert nv.errors == {'raceaian': ['error in formula evaluation - value 1 does not satisfy the specified formula']}
 
 def test_compatibility_with_nested_logic():
     """ Test when the rule has a nested compatibility - logic """
