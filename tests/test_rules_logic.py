0fabe3b3f1cb0c6a852b7efde94f65a77b5be3f9
Emily Cheng <echeng06@uw.edu>
2024-11-01 10:36:37 -0700

    Add some more test cases for common rules

--- tests/test_rules_logic.py
+++ tests/test_rules_logic.py
@@ -29,7 +29,6 @@ def test_logic_or(create_nacc_validator):
             }
         }
     }
-
     nv = create_nacc_validator(schema)
 
     # valid cases
@@ -73,7 +72,6 @@ def test_logic_and(create_nacc_validator):
             }
         }
     }
-
     nv = create_nacc_validator(schema)
 
     # only valid case
@@ -82,3 +80,86 @@ def test_logic_and(create_nacc_validator):
     # invalid cases
     assert not nv.validate({'raceaian': 1, 'raceasian': None, 'raceblack': None})
     assert nv.errors == {'raceaian': ['error in formula evaluation - value 1 does not satisfy the specified formula']}
+
+def test_logic_or_equality_with_none(create_nacc_validator):
+    """ Check logic can handle null values """
+    schema = {
+        "sib1yob": {"type": "integer", "nullable": True},
+        "sib2yob": {"type": "integer", "nullable": True},
+        "sib3yob": {"type": "integer", "nullable": True},
+        "sib4yob": {"type": "integer", "nullable": True},
+        "sib5yob": {"type": "integer", "nullable": True},
+        "ftdsibby": {
+            "type": "integer", "required": True,
+            "logic": {
+                "formula": {
+                    "or": [
+                        {
+                            "<=": [{"var": "ftdsibby"}, {"var": "sib1yob"}]
+                        },
+                        {
+                            "==": [{"var": "ftdsibby"}, {"var": "sib2yob"}]
+                        },
+                        {
+                            "==": [{"var": "ftdsibby"}, {"var": "sib3yob"}]
+                        },
+                        {
+                            "==": [{"var": "ftdsibby"}, {"var": "sib4yob"}]
+                        },
+                        {
+                            "==": [{"var": "ftdsibby"}, {"var": "sib5yob"}]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    nv = create_nacc_validator(schema)
+
+    assert nv.validate({'ftdsibby': 2000, 'sib1yob': 2000})
+    assert nv.validate({'ftdsibby': 2000, 'sib2yob': 2000})
+    assert nv.validate({'ftdsibby': 2000, 'sib3yob': 2000})
+    assert nv.validate({'ftdsibby': 2000, 'sib4yob': 2000})
+    assert nv.validate({'ftdsibby': 2000, 'sib5yob': 2000})
+    assert nv.validate({'ftdsibby': 2000, 'sib1yob': 1990 , 'sib2yob': 1991, 'sib3yob': 2000, 'sib4yob': 1993, 'sib5yob': 1994})
+    assert nv.validate({'ftdsibby': 2000, 'sib1yob': None , 'sib2yob': None, 'sib3yob': 2000})
+
+    assert not nv.validate({'ftdsibby': 2000, 'sib1yob': 1990 , 'sib2yob': 1991, 'sib3yob': 1992, 'sib4yob': 1993, 'sib5yob': 1994})
+    assert nv.errors == {'ftdsibby': ['error in formula evaluation - value 2000 does not satisfy the specified formula']}
+    assert not nv.validate({'ftdsibby': 2000, 'sib1yob': None , 'sib2yob': 1991, 'sib3yob': None, 'sib4yob': 1993, 'sib5yob': None})
+    assert nv.errors == {'ftdsibby': ['error in formula evaluation - value 2000 does not satisfy the specified formula']}
+    assert not nv.validate({'ftdsibby': 2000})
+    assert nv.errors == {'ftdsibby': ['error in formula evaluation - value 2000 does not satisfy the specified formula']}
+
+def test_logic_sum(create_nacc_validator):
+    """ Test must equal a sum logic, which is a common rule """
+    schema = {
+        "var1": {"type": "integer"},  # important to note, additive logic does NOT support null values, so either
+        "var2": {"type": "integer"},  # these need to be required (as done in this test), OR the logic needs
+        "var3": {"type": "integer"},  # to be wrapped in a compatibility rule that first checks they're not null
+        "total": {
+            "type": "integer",
+            "logic": {
+                "formula": {
+                    "==": [
+                        {"var": "total"},
+                        {
+                            "+": [
+                                {"var": "var1"},
+                                {"var": "var2"},
+                                {"var": "var3"}
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    nv = create_nacc_validator(schema)
+
+    assert nv.validate({"total": 10, "var1": 5, "var2": 3, "var3": 2})
+    assert nv.validate({"total": 10, "var1": 10, "var2": 0, "var3": 0})
+    assert nv.validate({"total": 10, "var1": 20, "var2": -5, "var3": -5})
+
+    assert not nv.validate({"total": 9, "var1": 5, "var2": 3, "var3": 2})
+    assert nv.errors == {'total': ['error in formula evaluation - value 9 does not satisfy the specified formula']}
