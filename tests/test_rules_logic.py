cd6d41b4f5c5d30279749da61401064034931414
Emily Cheng <echeng06@uw.edu>
2024-11-15 14:02:08 -0800

    Test count_exact none case

--- tests/test_rules_logic.py
+++ tests/test_rules_logic.py
@@ -218,6 +218,59 @@ def test_logic_count_exact(create_nacc_validator):
     assert not nv.validate({"count": 4, "var5": 1, "var4": None, "var3": None, "var2": 0, "var1": 0})
     assert nv.errors == {'count': ['error in formula evaluation - value 4 does not satisfy the specified formula']}
 
+def test_logic_count_exact_none(create_nacc_validator):
+    """ Test the count_exact logic rule against None """
+    schema = {
+        "var1": {"type": "integer", "nullable": True},
+        "var2": {"type": "integer", "nullable": True},
+        "var3": {"type": "integer", "nullable": True},
+        "var4": {"type": "integer", "nullable": True},
+        "var5": {"type": "integer", "nullable": True},
+        "count": {
+            "type": "integer",
+            "logic": {
+                "formula": {
+                    "==": [
+                        {
+                            "var": "count"
+                        },
+                        {
+                            "count_exact": [
+                                None,
+                                {"var": "var1"},
+                                {"var": "var2"},
+                                {"var": "var3"},
+                                {"var": "var4"},
+                                {"var": "var5"}
+                            ]
+                        }
+                    ]
+                }
+            }
+        }
+    }
+    nv = create_nacc_validator(schema)
+
+    # valid case
+    assert nv.validate({"count": 5, "var1": None, "var2": None, "var3": None, "var4": None, "var5": None})
+    assert nv.validate({"count": 4, "var1": None, "var2": None, "var3": None, "var4": None, "var5": 1})
+    assert nv.validate({"count": 3, "var1": None, "var2": None, "var3": 0, "var4": None, "var5": 1})
+    assert nv.validate({"count": 0, "var1": 1, "var2": 2, "var3": 3, "var4": 4, "var5": 5})
+
+    # make sure it handles valid blank/null case
+    assert nv.validate({"count": 5})
+    assert nv.validate({"count": 2, "var5": 0, "var4": 1, "var3": 0})
+    assert nv.validate({"count": 2, "var5": 0, "var4": None, "var3": None, "var2": 0, "var1": 0})
+
+    # invalid cases
+    assert not nv.validate({"count": 5, "var1": 0, "var2": 0, "var3": 0, "var4": 0, "var5": 0})
+    assert nv.errors == {'count': ['error in formula evaluation - value 5 does not satisfy the specified formula']}
+    assert not nv.validate({"count": 5, "var1": None, "var2": 2, "var3": None, "var4": 4, "var5": None})
+    assert nv.errors == {'count': ['error in formula evaluation - value 5 does not satisfy the specified formula']}
+
+    assert not nv.validate({"count": 2, "var5": 1, "var2": 0, "var1": None})
+    assert nv.errors == {'count': ['error in formula evaluation - value 2 does not satisfy the specified formula']}
+
 def test_logic_count_exact_invalid_list(create_nacc_validator):
     """ Checking an error is thrown if not enough arguments are passed to count_exact """
     schema = {
