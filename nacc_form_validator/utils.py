863b7dc7596687e95e0467d736175ac1abe23f1b
Ben Keller <bjkeller@vt.edu>
2025-05-22 22:01:48 +0000

    Refactor utilities

--- nacc_form_validator/utils.py
+++ nacc_form_validator/utils.py
@@ -3,14 +3,15 @@
 import logging
 import math
 import re
-from typing import Any
+from datetime import date, datetime
+from typing import Any, Optional
 
 from dateutil import parser
 
 log = logging.getLogger(__name__)
 
 
-def convert_to_date(value) -> Any:
+def convert_to_date(value: Any) -> date:
     """Convert the input value to date object.
 
     Args:
@@ -33,7 +34,7 @@ def convert_to_date(value) -> Any:
         raise parser.ParserError(error) from error
 
 
-def convert_to_datetime(value) -> Any:
+def convert_to_datetime(value: Any) -> datetime:
     """Convert the input value to datetime object.
 
     Args:
@@ -58,7 +59,14 @@ def convert_to_datetime(value) -> Any:
         raise parser.ParserError(error) from error
 
 
-def compare_values(comparator: str, value: object, base_value: object) -> bool:
+def get_float(value: Any) -> Optional[float]:
+    try:
+        return float(value)
+    except (TypeError, ValueError):
+        return None
+
+
+def compare_values(comparator: str, value: Any, base_value: Any) -> bool:
     """Compare two values.
 
     Args:
@@ -69,49 +77,39 @@ def compare_values(comparator: str, value: object, base_value: object) -> bool:
     Returns:
         bool: True if the formula is satisfied, else False
     """
-    # try close enough equality if both are floats first
-    both_floats = False
-    if isinstance(value, (str, int, float)) \
-        and isinstance(base_value, (str, int, float)):
-        try:
-            float(value)  # don't actually set it to a in case we die at b
-            float(base_value)
-            both_floats = True
-        except ValueError:
-            pass
-
-    # test these first as they don't care about null values
-    if comparator == "==":
-        return value == base_value if not both_floats else \
-            math.isclose(float(value), float(base_value), abs_tol=1e-2)
-
-    if comparator == "!=":
-        return value != base_value if not both_floats else \
-            not math.isclose(float(value), float(base_value), abs_tol=1e-2)
-
-    if comparator not in ["<=", ">=", "<", ">"]:
+    if comparator not in ["==", "!=", "<=", ">=", "<", ">"]:
         raise TypeError(f"Unrecognized comparator: {comparator}")
 
     # for < and >, follow same convention as jsonlogic for null values
     # for >= and <=, allow equality case (both None)
     if value is None and base_value is None:
-        return True if comparator in ["<=", ">="] else False
+        return True if comparator in ["<=", "==", ">="] else False
     if value is None:
         return True if comparator in ["<", "<="] else False
     if base_value is None:
         return False if comparator in ["<", "<="] else True
 
+    # try close enough equality if both are floats
+    float_value = get_float(value)
+    float_base_value = get_float(base_value)
+    if float_value is not None and float_base_value is not None:
+        comp = math.isclose(float_value, float_base_value, abs_tol=1e-2)
+        if comparator == "==":
+            return comp
+
+        if comparator == "!=":
+            return not comp
+
     # now try as normal
+    if comparator == "==":
+        return value == base_value
+    if comparator == "!=":
+        return value != base_value
     if comparator == ">=":
         return value >= base_value
-
     if comparator == ">":
         return value > base_value
-
     if comparator == "<=":
         return value <= base_value
-
     if comparator == "<":
         return value < base_value
-
-    raise TypeError(f"Unrecognized comparator: {comparator}")
